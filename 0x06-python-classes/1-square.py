5165a96789e22c88fd176d9d49cd75174036a085
debbiejeruto <debbykemboi99@gmail.com>
2022-06-21 13:35:42 -0700

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""Square class to represent a square"""
+""" Working on oop with python """
 
 
 class Square():
-    """Empty class"""
-    pass
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """ initialize the class
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
